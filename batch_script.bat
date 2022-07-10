4b43c63eb4d9dd6632c46a5bc8f98320f5cc2ef6
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2022-07-10 10:25:40 +0800

    Update batch_script.bat

--- batch_script.bat
+++ batch_script.bat
@@ -1,1 +1,7 @@
 ren C:\lists.txt C:\newlists.txt rem file name change
+start notepad.exe rem start application on windows 
+echo %TIME% rem 9:40:15.24 tsag minute second, doiliig xaruulna
+SET /A a=5
+SET /A b=10
+SET /A c=%a% + %b%
+echo %c% rem add two numbers
