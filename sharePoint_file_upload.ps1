c79d164eab085f8480f775b39a9207126233de61
Myagmardorj <67570666+zapataast@users.noreply.github.com>
2022-11-28 16:16:36 +0800

    sharepoint to store files automative

--- sharePoint_file_upload.ps1
+++ sharePoint_file_upload.ps1
@@ -1,22 +1,65 @@
-
 $URL = "https://altanjoloogroupmn.sharepoint.com/sites/it77"
-$TargetFolderRelativeURL ="/Shared Documents/Backups"
-$sourcefile = "D:\sharepoint_sync\gg.txt"
-
+$TargetFolderRelativeURL ="/Shared Documents/Backups/Test"
+$sourceFolder = "D:\sharepoint_sync"
 
 $Cred = Get-Credential
 Import-Module SharePointPnPPowerShellOnline
 Connect-PnPOnline -url $URL
-
-$Files = Get-ChildItem "D:\sharepoint_sync" -Recurse | % { $_.FullName }
+#region subfolders create 
+<#
+$get_all_sub_folders = Get-ChildItem -Path $sourceFolder -Recurse -Directory -Force -ErrorAction SilentlyContinue | Select-Object FullName
+$main_path = ($sourceFolder -split "\\")[-1]
+foreach($f in $get_all_sub_folders){
+    $tmp1 = ($f.FullName -split "\\")[-1]   # subfolders name creating
+    if(($f.FullName -split "\\")[-2] -eq $main_path){
+        Add-PnPFolder -Name $tmp1 -Folder $TargetFolderRelativeURL #CREATING SUB FOLDERS
+    }else{
+        $tmp2 = $f.FullName.Substring($sourceFolder.Length)
+        $tmp2 = $tmp2.Substring(1,$tmp2.Length-$tmp1.Length-2)
+        $tmp2 = $tmp2.Replace('\','/')
+        $target = $TargetFolderRelativeURL + "/" + $tmp2  # subfolders url path
+        Add-PnPFolder -Name $tmp1 -Folder $target   #CREATING SUB FOLDERS
+        #Start-Sleep -Seconds 1
+    }
+}
+#>
+#endregion
+#region subfiles upload
+$Files = Get-ChildItem $sourceFolder -Recurse | % { $_.FullName }
 foreach($File in $Files) {
     $tempdate = (Get-Item $File).CreationTime
     #Write-Output $tempdate
-    if((Get-Date).Day -eq $tempdate.Day){
-        Add-PnPFile -Path $temppath -Folder $TargetFolderRelativeURL -ErrorAction Stop
-    }
-    Write-Output $File
-    #Add-PnPFile -Path $sourcefile -Folder $TargetFolderRelativeURL -ErrorAction Stop
+    $tmp1 = ($File -split "\\")[-1]
+    #if((Get-Date).Day -eq $tempdate.Day){
+        if(($File -split "\\")[-2] -eq $main_path)
+        {
+            if(Test-Path -Path $File -PathType Leaf){
+                try {
+                    Add-PnPFile -Path $File -Folder $TargetFolderRelativeURL -ErrorAction Stop 
+                    Write-Output $File "file uploaded"
+                }
+                catch {
+                    Write-Output $File "file passed"
+                }
+                
+            }
+        }else{
+            $tmp2 = $File.Substring($sourceFolder.Length)
+            $tmp2 = $tmp2.Substring(1,$tmp2.Length-$tmp1.Length-2)
+            $tmp2 = $tmp2.Replace('\','/')
+            $target = $TargetFolderRelativeURL + "/" + $tmp2  # subfolders url path
+            if(Test-Path -Path $File -PathType Leaf){
+                try {
+                    Add-PnPFile -Path $File -Folder $target   #CREATING SUB FOLDERS
+                    Write-Output $tmp1+"file uploaded"
+                }
+                catch {
+                    Write-Output $File "passed"
+                }
+                
+            }
+        }        
+#    }
 }
+#endregion
 
-$gg = Get-ChildItem D:*.txt -Filter *.txt -Recurse | % { $_.FullName }
